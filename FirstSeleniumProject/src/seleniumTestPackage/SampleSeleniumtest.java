3425896c966caa47bcfd36cf129478d49fd3aed3
Admin <Admin@Admin-PC>
2018-12-08 14:12:59 +0530

    Added chromeDriver Systemproperty line

--- FirstSeleniumProject/src/seleniumTestPackage/SampleSeleniumtest.java
+++ FirstSeleniumProject/src/seleniumTestPackage/SampleSeleniumtest.java
@@ -1,17 +1,18 @@
 package seleniumTestPackage;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SampleSeleniumtest {
 
 	public static void main(String[] args) {
 		String projectPath = System.getProperty("user.dir");
-		System.setProperty("webdriver.gecko.driver",projectPath+"\\lib\\geckodriver\\geckodriver.exe");
-		////System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox");
+		//System.setProperty("webdriver.gecko.driver",projectPath+"\\lib\\geckodriver\\geckodriver.exe");
+		//System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox");
 		//WebDriver driver = new MarionetteDriver();
-		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver",projectPath+"\\lib\\chromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.get("http://seleniumhq.org/");
 		driver.quit();
 	} 
