eccaa85b2d2e850d6071fe4b4f917536a42bf7b0
Admin <Admin@Admin-PC>
2018-12-09 12:28:47 +0530

    Created modules in the main class

--- FirstSeleniumProject/src/seleniumTestPackage/SampleSeleniumtest.java
+++ FirstSeleniumProject/src/seleniumTestPackage/SampleSeleniumtest.java
@@ -2,19 +2,54 @@ package seleniumTestPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SampleSeleniumtest {
-
+		static String browser;
+		static WebDriver driver;
+		
+		
 	public static void main(String[] args) {
+		setBrowser();
+		setBrowserConfig();
+		runTest();
+	} 
+	
+	//setBrowser
+	//setBrowserConfig
+	//runTest
+	
+	/**
+	 * This Modules are to setBrowser and Brower configuration
+	 * @author Divya
+	 */
+	
+	public static void setBrowser() {
+		browser="Chrome";
+		
+	}
+	
+	public static void setBrowserConfig() {
 		String projectPath = System.getProperty("user.dir");
-		//System.setProperty("webdriver.gecko.driver",projectPath+"\\lib\\geckodriver\\geckodriver.exe");
-		//System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox");
-		//WebDriver driver = new MarionetteDriver();
-		//WebDriver driver = new FirefoxDriver();
-		System.setProperty("webdriver.chrome.driver",projectPath+"\\lib\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		
+		//This condition block sets browser configuration
+		if(browser.contains("Firefox")) {
+			System.setProperty("webdriver.gecko.driver",projectPath+"\\lib\\geckodriver\\geckodriver.exe");
+			//System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox");
+			//WebDriver driver = new MarionetteDriver();
+			driver = new FirefoxDriver();
+		}
+		
+		if(browser.contains("Chrome")) {
+			System.setProperty("webdriver.chrome.driver",projectPath+"\\lib\\chromeDriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+	}
+	
+	
+	public static void runTest() {
 		driver.get("http://seleniumhq.org/");
 		driver.quit();
-	} 
+	}
 
 }
